c7d2b4c757b8cc9efafdf80c5aa64ce52721684c
Bhramarv021 <bhramarv021@gmail.com>
2021-08-17 21:30:44 +0530

    Selection Sort Program

---
+++ Sorting/SelectionSortProgram.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class SelectionSortProgram{
+	public static void main(String[] args) {
+		int n;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Size of array : ");
+		n = sc.nextInt();
+
+		int[] arr = new int[n];
+		System.out.println("Enter "+n+" elements");
+		for (int i=0; i<n ; i++ ) {
+			arr[i] = sc.nextInt();
+		}
+
+		sortUsingSelectionSort(arr, n);
+	}
+	private static void sortUsingSelectionSort(int[] arr, int n){
+		for (int i=0; i<n-1 ; i++ ) {
+			for (int j=i+1 ; j<n ; j++ ) {
+				if(arr[j] < arr[i]){
+					// smallest = arr[j];
+					// index = j;
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+
+		System.out.print("Sorted Array : ");
+		for (int i=0; i<n ; i++)
+			System.out.print(arr[i] + " ");
+	}
+}
