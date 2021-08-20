80e77ef2bd769cb6804b37309f6e95a0e7ed89f2
Bhramarv021 <bhramarv021@gmail.com>
2021-08-20 18:26:38 +0530

    Merge Sort Program

--- Sorting/MergeSortProgram.java
+++ Sorting/MergeSortProgram.java
@@ -1,5 +1,74 @@
 class MergeSortProgram{
 	public static void main(String[] args) {
+		int arr[] = {2,7,4,1,5,3,9,6};
+		for (int i=0; i<arr.length ; i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+		mergeSort(arr);
+	}
+
+	private static void mergeSort(int[] arr){
+		int n = arr.length;
+		
+		if (n<2) {
+			return;
+		}
+
+		int mid = n/2;
+		int[] left = new int[mid];
+		int[] right = new int[n-mid];
+
+		for (int i=0; i<mid ; i++) {
+			left[i] = arr[i];
+		}
+
+		for (int i=mid; i<n ; i++) {
+			right[i-mid] = arr[i];
+		}
+
+		mergeSort(left);
+		mergeSort(right);
+		merge(left, right, arr);
+
+		for (int i=0; i<n ; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+
+	private static void merge(int[] leftArr, int[] rightArr, int[] arr){
+		int leftArrLength = leftArr.length;
+		int rightArrLength = rightArr.length;
+		int i,j,k;
+		i = j = k = 0;
+		while (i<leftArrLength && j<rightArrLength) {
+			if (leftArr[i] <= rightArr[j]) {
+				arr[k] = leftArr[i];
+				i++;
+				k++;
+			}
+			else{
+				arr[k] = rightArr[j];
+				j++;
+				k++;
+			}
+		}
+		while(i<leftArrLength){
+			arr[k] = leftArr[i];
+			i++;
+			k++;
+		}
+		while(j<rightArrLength){
+			arr[k] = rightArr[j];
+			j++;
+			k++;
+		}
+	}
+}
+
+/*class MergeSortProgram{
+	public static void main(String[] args) {
 		int[] arr = {5,4,3,2,1};
 		mergeSort(arr, 0, 4);
 
@@ -62,4 +131,4 @@ class MergeSortProgram{
 			j++;
 		}
 	}
-}
+}*/
