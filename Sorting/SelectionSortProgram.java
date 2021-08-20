46b4d524dd9c6f9e1d9b70b7e8d5541340558959
Bhramarv021 <bhramarv021@gmail.com>
2021-08-20 18:26:20 +0530

    Selection Sort Program

--- Sorting/SelectionSortProgram.java
+++ Sorting/SelectionSortProgram.java
@@ -18,8 +18,6 @@ class SelectionSortProgram{
 		for (int i=0; i<n-1 ; i++ ) {
 			for (int j=i+1 ; j<n ; j++ ) {
 				if(arr[j] < arr[i]){
-					// smallest = arr[j];
-					// index = j;
 					int temp = arr[i];
 					arr[i] = arr[j];
 					arr[j] = temp;
