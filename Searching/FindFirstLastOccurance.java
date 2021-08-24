26a6a00091a9ea765025bf701a63fb0009f451e4
Bhramarv021 <bhramarv021@gmail.com>
2021-08-24 18:12:48 +0530

    Q -> Find first and last occurance of a number in sorted list

--- Searching/FindFirstLastOccurance.java
+++ Searching/FindFirstLastOccurance.java
@@ -3,16 +3,14 @@
 import java.util.Scanner;
 class FindFirstLastOccurance{
 	public static void main(String[] args) {
-		// int[] arr = {2,4,10,10,10,18,20};
-		int[] arr = {1,1,3,3,5,5,5,5,5,9,9,11};
+		int[] arr = {2,4,10,10,10,18,20};
 		int n = arr.length;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter number you want to search : ");
 		int se = sc.nextInt();
 		int firstOccurance = findFirstOccurance(arr,n,se);
 		int lastOccurance = findLastOccurance(arr, n, se);		
-		int totalNum = lastOccurance - firstOccurance + 1;
-		System.out.println(firstOccurance + " " + lastOccurance + " " + totalNum);
+		System.out.println(firstOccurance + " " + lastOccurance);
 	}
 
 	private static int findFirstOccurance(int[] arr, int n, int se){
@@ -24,7 +22,6 @@ class FindFirstLastOccurance{
 			if (arr[mid] == se) {
 				result = mid;
 				high = mid-1;
-				System.out.println("R : "+result);
 			}
 			else if (arr[mid] > se) {
 				high = mid-1;
